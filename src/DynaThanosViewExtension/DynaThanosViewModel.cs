75220c6e2b17303e4270f3e6582a822a61cdd8bf
johnpierson <sixtysecondrevit@gmail.com>
2019-03-29 07:35:45 -0600

    InitialUpload
    Initial upload of package

---
+++ src/DynaThanosViewExtension/DynaThanosViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Dynamo.Controls;
+using Dynamo.Core;
+using Dynamo.Extensions;
+using Dynamo.Graph.Nodes;
+using Dynamo.Models;
+using Dynamo.ViewModels;
+using Dynamo.Wpf.Extensions;
+using Xceed.Wpf.AvalonDock.Controls;
+
+namespace DynaThanosViewExtension
+{
+    public class DynaThanosViewModel : NotificationObject
+    {
+        private string activeNodeTypes;
+        private static ViewLoadedParams loadedParams;
+        private static NodeModel _currentNode;
+        private static DynamoViewModel _vm;
+
+        public DynaThanosViewModel(ViewLoadedParams p)
+        {
+            loadedParams = p;
+            _vm = loadedParams.DynamoWindow.DataContext as DynamoViewModel;
+        }
+
+       
+
+        public static void RespondToSnap()
+        {
+            var nodes = _vm.CurrentSpace.Nodes.ToList();
+
+            int flag = 0;
+            var nodeViews = loadedParams.DynamoWindow.FindVisualChildren<NodeView>();
+
+            while (flag < nodes.Count())
+            {
+                _currentNode = nodes[flag];
+                var nodeView = nodeViews.First(n => n.ViewModel.Name.Equals(_currentNode.Name));
+
+                DoubleAnimation animation = new DoubleAnimation(0, TimeSpan.FromSeconds(3));
+                nodeView.BeginAnimation(Control.OpacityProperty, animation);
+                flag++;
+            }
+
+
+
+        }
+
+       
+    }
+}
---
+++ src/DynaThanosViewExtension/DynaThanosWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using Dynamo.Graph.Nodes;
+using Dynamo.ViewModels;
+
+namespace DynaThanosViewExtension
+{
+    /// <summary>
+    /// Interaction logic for SampleWindow.xaml
+    /// </summary>
+    public partial class DynaThanosWindow : Window
+    {
+        public DynaThanosWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+                DynaThanosViewModel.RespondToSnap();
+            
+           
+        }
+    }
+}
